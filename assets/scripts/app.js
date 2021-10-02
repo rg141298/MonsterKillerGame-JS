0005246e9aae51f7508d358eebb5e19ff174f497
Raman Goyal <ramangoyal@192.168.0.8>
2021-10-02 11:18:54 +0530

    Function to attack monster and and monster hits the player back with some power

---
+++ assets/scripts/app.js
@@ -0,0 +1,25 @@
+const ATTACK_VALUE = 10;
+const Monster_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+//Function is in vendor.js
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE); /* Function is in vendor.js*/
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(Monster_ATTACK_VALUE) /*Function is in vendor.js */
+  currentPlayerHealth -= playerDamage
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth >0 ) {
+    alert("You won!");
+  } else if(currentPlayerHealth <=0 && currentMonsterHealth > 0){
+      alert('You lost!');
+  } else if (currentMonsterHealth<=0 && currentPlayerHealth <=0){
+      alert ('You have a draw');
+  }
+}
+attackBtn.addEventListener("click", attackHandler);
