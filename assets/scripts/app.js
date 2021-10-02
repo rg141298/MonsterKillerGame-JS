2f610760db273f1bb956dcf966deb1c4940eea2b
Raman Goyal <ramangoyal@192.168.0.8>
2021-10-02 11:23:13 +0530

    Function for Strong  attack on the monster

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 18;
 const Monster_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -11,15 +12,35 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE); /* Function is in vendor.js*/
   currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(Monster_ATTACK_VALUE) /*Function is in vendor.js */
-  currentPlayerHealth -= playerDamage
+  const playerDamage =
+    dealPlayerDamage(Monster_ATTACK_VALUE); /*Function is in vendor.js */
+  currentPlayerHealth -= playerDamage;
 
-  if (currentMonsterHealth <= 0 && currentPlayerHealth >0 ) {
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You won!");
-  } else if(currentPlayerHealth <=0 && currentMonsterHealth > 0){
-      alert('You lost!');
-  } else if (currentMonsterHealth<=0 && currentPlayerHealth <=0){
-      alert ('You have a draw');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("You have a draw");
   }
 }
+
+function strongAttackHandler() {
+  const damage =
+    dealMonsterDamage(STRONG_ATTACK_VALUE); /* Function is in vendor.js*/
+  currentMonsterHealth -= damage;
+  const playerDamage =
+    dealPlayerDamage(Monster_ATTACK_VALUE); /*Function is in vendor.js */
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("You have a draw");
+  }
+}
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
